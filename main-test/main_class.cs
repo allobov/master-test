75bbd7effbeb250d9d045cda18c4413c407a9eb2
Lobov Alexander <allobov.al@gmail.com>
2021-04-20 00:19:50 +0300

    Going online

---
+++ main-test/main_class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace master_main
+{
+    class main_class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is master main");
+            Console.ReadKey();
+        }
+    }
+}
